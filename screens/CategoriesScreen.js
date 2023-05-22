7aac8eb1f23775b803c62bb53373e08b49ebb489
azne <a_kurbanov@hotmail.com>
2023-05-22 21:43:49 +0200

    update

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,13 +1,14 @@
-import CategoryGridTile from "../components/CategoryGridTile";
-import { CATEGORIES } from "../data/dummy-data";
-import { Text, FlatList } from "react-native";
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+import { CATEGORIES } from '../data/dummy-data';
 
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-        navigation.navigate("MealsOverview", {
-            categoryId: itemData.item.id,
-        });
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id,
+      });
     }
 
     return (
@@ -18,6 +19,7 @@ function CategoriesScreen({ navigation }) {
       />
     );
   }
+
   return (
     <FlatList
       data={CATEGORIES}
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,23 +1,47 @@
-import { StyleSheet, View, FlatList } from "react-native";
+import { useLayoutEffect } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
 
-import { MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
+import MealItem from '../components/MealItem';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
-  const displayMeals = MEALS.filter((mealItem)=>{
-    return mealItem.categoryIds.indexOf(catId) >=0;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
   });
 
-  function renderMealItem(itemData){
-    return <MealItem title={itemData.item.title}/>
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
 
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
     <View style={styles.container}>
-      <FlatList data={displayMeals} keyExtractor={(item)=>item.id} renderItem={renderMealItem}/>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
